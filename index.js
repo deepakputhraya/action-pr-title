2ddf988eadd0ae718829810db480a4facca4251d
James Stremick <james.stremick@vonage.com>
2020-06-15 13:48:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function validateTitlePrefix(title, prefix, caseSensitive) {
         prefix = prefix.toLowerCase();
         title = prefix.toLowerCase();
     }
+    core.info(`Checking: ${title} starts with ${prefix} - ${title.startsWith(prefix)`);
     return title.startsWith(prefix);
 }
 
